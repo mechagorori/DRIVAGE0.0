902e95f50af96503b8fc52b9b2d5ae22102a650e
Takumi Kobayashi <t.kobayashi@inside.tworks.co.jp>
2022-11-02 15:36:27 +0900

    const 修正

--- packages/app/src/const/car.ts
+++ packages/app/src/const/car.ts
@@ -1,9 +1,30 @@
 export const StandardCar = {
-  title: "",
-  image: "",
-  price: 10,
-  maxHp: 100,
-  currentHp: 100,
-  repairPrice: 5,
-  attribute: null,
+  address: "",
+  meta: {
+    name: "",
+    description: "",
+    image: "",
+    attributes: [
+      {
+        trait_type: "price",
+        value: "0.01",
+      },
+      {
+        trait_type: "maxHp",
+        value: 100,
+      },
+      {
+        trait_type: "currentHp",
+        value: 100,
+      },
+      {
+        trait_type: "repairPrice",
+        value: 5,
+      },
+      {
+        trait_type: "attribute",
+        value: null,
+      },
+    ],
+  },
 };

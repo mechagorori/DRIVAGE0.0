7cd16a8b4c359f1132173cf46e70fd088de52721
Takumi Kobayashi <t.kobayashi@inside.tworks.co.jp>
2022-11-03 12:48:16 +0900

    環境整備

--- packages/app/App.tsx
+++ packages/app/App.tsx
@@ -1,8 +1,28 @@
+import "./global";
+import "./shim";
+import { useEffect } from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import AuthClient from "@walletconnect/auth-client";
 import "./src/infrastructure/firebase";
 
 export default function App() {
+  useEffect(() => {
+    AuthClient.init({
+      relayUrl: "wss://relay.walletconnect.com",
+      projectId: "5290f7b69e00c31799bec49e5d9d02d2",
+      metadata: {
+        name: "react-dapp-auth",
+        description: "React Example Dapp for Auth",
+        url: "",
+        icons: [],
+      },
+    })
+      .then((authClient) => {
+        console.log(authClient);
+      })
+      .catch(console.error);
+  }, []);
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>

389722ab733cb5fe48982aaef30e6f06b1ca7f24
skk5523a <skk5523.a@gmail.com>
2022-11-06 06:48:04 +0900

    Add DrivageFactory.sol

--- packages/contracts/contracts/DrivageFactory.sol
+++ packages/contracts/contracts/DrivageFactory.sol
@@ -1,37 +1,37 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.17;
-import "./Dcoin.sol";
-import "./ANZNScore.sol";
+// // SPDX-License-Identifier: UNLICENSED
+// pragma solidity ^0.8.17;
+// import "./Dcoin.sol";
+// import "./ANZNScore.sol";
 
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
+// import "@openzeppelin/contracts/access/Ownable.sol";
+// import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract DrivageFactory is Ownable {
+// contract DrivageFactory is Ownable {
 
-    uint256 MAX_ANZN_PER_PLAY = 5;
-    constructor () {}
+//     uint256 MAX_ANZN_PER_PLAY = 5;
+//     constructor () {}
 
-    // Initialization by each player, creating an ANZN-token wallet
-    function initializeUserInfo () public {
-        ANZNScore.mint();
-    }
+//     // Initialization by each player, creating an ANZN-token wallet
+//     function initializeUserInfo () public {
+//         ANZNScore.mint();
+//     }
 
-    function updateBalances (uint256 driveScore) public {
-        require(driveScore <= MAX_ANZN_PER_PLAY, "The submitted score is invalid, exceeding the highest score.");
+//     function updateBalances (uint256 driveScore) public {
+//         require(driveScore <= MAX_ANZN_PER_PLAY, "The submitted score is invalid, exceeding the highest score.");
 
-        // As of the hackathon, players earn Dcoin and ANZN, both equivalently to the score itself
-        uint256 rewardAnzn = driveScore;
-        uint256 rewardDcoin = rewardAnzn;
-        Dcoin.mint(msg.sender, rewardDcoin);
-        ANZNScore.addAnzn(rewardAnzn);
-    }
+//         // As of the hackathon, players earn Dcoin and ANZN, both equivalently to the score itself
+//         uint256 rewardAnzn = driveScore;
+//         uint256 rewardDcoin = rewardAnzn;
+//         Dcoin.mint(msg.sender, rewardDcoin);
+//         ANZNScore.addAnzn(rewardAnzn);
+//     }
 
-    function consumeDcoin (uint256 amount) public {
-        Dcoin.burn(amount);
-    }
+//     function consumeDcoin (uint256 amount) public {
+//         Dcoin.burn(amount);
+//     }
 
-    function changeMaxAnznPerPlay (uint256 newMax) public onlyOwner {
-        MAX_ANZN_PER_PLAY = newMax;
-    }
-}
+//     function changeMaxAnznPerPlay (uint256 newMax) public onlyOwner {
+//         MAX_ANZN_PER_PLAY = newMax;
+//     }
+// }
 
--- packages/contracts/scripts/deploy.ts
+++ packages/contracts/scripts/deploy.ts
@@ -1,12 +1,26 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const CarNFT = await ethers.getContractFactory("CarNFT");
-  const carNFT = await CarNFT.deploy();
+//   const CarNFT = await ethers.getContractFactory("CarNFT");
+//   const carNFT = await CarNFT.deploy();
 
-  await carNFT.deployed();
+//   await carNFT.deployed();
 
-  console.log("CarNFT contract deployed to:", carNFT.address);
+//   console.log("CarNFT contract deployed to:", carNFT.address);
+
+  const ANZNScore = await ethers.getContractFactory("ANZNScore");
+  const anznScore = await ANZNScore.deploy();
+
+  await anznScore.deployed();
+
+  console.log("ANZN contract deployed to:", anznScore.address);
+
+  const Dcoin = await ethers.getContractFactory("Dcoin");
+  const dcoin = await Dcoin.deploy();
+
+  await dcoin.deployed();
+
+  console.log("Dcoin contract deployed to:", dcoin.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
